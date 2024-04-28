83628113cfddc2456b0d823d7977830a1868098f
lisa erksn <lisa.erksn@gmail.com>
2024-04-28 02:39:32 +0200

    Add files

---
+++ JS/team.js
@@ -0,0 +1,19 @@
+import { animateLogo } from "./functions.js"
+import { apparitionNavbar } from "./functions.js"
+import { banniereBtn } from "./functions.js"
+import { opacityVideo } from "./functions.js"
+import { popupOpen } from "./functions.js"
+import { popupClose } from "./functions.js"
+import { calculHeightBanniere } from "./functions.js"
+
+
+animateLogo()
+apparitionNavbar()
+banniereBtn()
+opacityVideo()
+popupOpen()
+popupClose()
+calculHeightBanniere()
+
+
+
